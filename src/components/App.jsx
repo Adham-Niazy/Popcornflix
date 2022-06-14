5d8398082d30808014e11f5bee1b8223e0b3f1ee
Adham-Niazy <adhamniazy@gmail.com>
2022-06-14 11:48:21 +0200

    Adding custom eslint configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
+function App() {
   return (
     <div>App</div>
-  )
+  );
 }
 
-export default App
+export default App;
