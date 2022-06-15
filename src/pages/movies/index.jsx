2ece4f2e22efdad4cc18bd45fe48aa2e234b9709
Adham-Niazy <adhamniazy@gmail.com>
2022-06-16 01:20:07 +0200

    Rendering Movies + Finishing Movie View

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -3,15 +3,33 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MoviesList } from '../../components';
 
-function MoviesList() {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+function Movies() {
+  const { data, error, isFetching } = useGetMoviesQuery();
+  // Loading State
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+  // Empty State
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4"> No movies that match that name. <br /> Please search for something else. </Typography>
+      </Box>
+    );
+  }
+  // Error State
+  if (error) return 'An error has occured.';
   return (
     <div>
-      <MoviesList />
+      <MoviesList movies={data} />
     </div>
   );
 }
 
-export default MoviesList;
+export default Movies;
