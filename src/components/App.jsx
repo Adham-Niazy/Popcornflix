62433b9bb1cbca882c4b74cd74703d67d8d787fe
Adham-Niazy <adhamniazy@gmail.com>
2022-06-18 04:20:53 +0200

    Finalizing Authentication

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,6 +37,7 @@ function App() {
           <Route path="/movie/:id" element={<MovieInformation />} />
           <Route path="/actors/:id" element={<Actors />} />
           <Route path="/profile/:id" element={<Profile />} />
+          <Route path="*" element={<Movies />} />
         </Routes>
       </main>
     </div>
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,8 +1,35 @@
+import { Box, Typography, Button } from '@mui/material';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 function Profile() {
+  const { user } = useSelector((state) => state.user);
+
+  const favoriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display="flex" justifyContent="space-between" alignItems="center" style={{ marginBottom: '1rem' }}>
+        <Typography variant="h4">My Profile - {user.username}</Typography>
+        <Button variant="outlined" color="error" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {
+        !favoriteMovies.length ? (
+          <Typography variant="h5">Add favorite or watchlist some movies to see them here!</Typography>
+        ) : (
+          <Box>
+            Favorite Movies
+          </Box>
+        )
+      }
+    </Box>
   );
 }
 
